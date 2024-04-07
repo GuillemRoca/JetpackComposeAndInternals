89a3ccb65d42e54137f0a286f80866731d8c598e
Guillem Roca <10449477+GuillemRoca@users.noreply.github.com>
2024-04-07 19:44:07 +0200

    Revert "Complete exercise 1"
    This reverts commit d187c6c73661d561e50681fd4581431bdb353653.

--- app/src/main/java/dev/jorgecastillo/compose/app/ui/composables/NamePlate.kt
+++ app/src/main/java/dev/jorgecastillo/compose/app/ui/composables/NamePlate.kt
@@ -11,9 +11,7 @@ import dev.jorgecastillo.compose.app.ui.theme.ComposeAndInternalsTheme
 
 @Composable
 fun NamePlate(name: String) {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text(text = name)
-    }
+
 }
 
 @Composable
