8a685f0f2b7da3fcadbdc16828b764f6dca1ea2f
Guillem Roca <10449477+GuillemRoca@users.noreply.github.com>
2024-04-29 18:50:34 +0200

    Complete exercise 9

--- app/src/androidTest/java/dev/jorgecastillo/compose/app/Theme.kt
+++ app/src/androidTest/java/dev/jorgecastillo/compose/app/Theme.kt
@@ -16,10 +16,24 @@ private val DarkColors = darkColors(
     secondary = Teal200
 )
 
+private val DarkPinkColors = darkColors(
+    primary = Pink200,
+    primaryVariant = Pink700,
+    secondary = Teal200,
+    surface = Pink200
+)
+
 private val LightColors = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
     secondary = Teal200
+)
+
+private val LightPinkColors = lightColors(
+    primary = Pink500,
+    primaryVariant = Pink700,
+    secondary = Teal200,
+    surface = Pink700
 )
 
 @Composable
@@ -31,6 +45,25 @@ fun MyAppTheme(
         DarkColors
     } else {
         LightColors
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
+
+@Composable
+fun PinkTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkPinkColors
+    } else {
+        LightPinkColors
     }
 
     MaterialTheme(
