ac98e72b1bd44bd915e185f15545bc27a99d088c
Guillem Roca <10449477+GuillemRoca@users.noreply.github.com>
2024-06-17 10:03:13 +0200

    Complete exercise 12. Course completed

--- app/src/main/java/dev/jorgecastillo/compose/app/MainActivity.kt
+++ app/src/main/java/dev/jorgecastillo/compose/app/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.graphics.ExperimentalAnimationGraphicsApi
+import dev.jorgecastillo.compose.app.ui.composables.MainScreen
 import dev.jorgecastillo.compose.app.ui.composables.NamePlate
 import dev.jorgecastillo.compose.app.ui.composables.SpeakersRecompositionScreen
 import dev.jorgecastillo.compose.app.ui.theme.ComposeAndInternalsTheme
@@ -16,7 +17,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeAndInternalsTheme {
-                SpeakersRecompositionScreen()
+                MainScreen()
             }
         }
     }
--- app/src/main/java/dev/jorgecastillo/compose/app/ui/composables/MainScreen.kt
+++ app/src/main/java/dev/jorgecastillo/compose/app/ui/composables/MainScreen.kt
@@ -1,16 +1,46 @@
 package dev.jorgecastillo.compose.app.ui.composables
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import dev.jorgecastillo.compose.app.data.FakeSpeakerRepository
 
 @Composable
 fun MainScreen() {
-    // Write your code here
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text("Speakers") })
+        },
+    ) { paddingValues ->
+        val navController = rememberNavController()
+        NavHost(
+            modifier = Modifier.padding(paddingValues),
+            navController = navController,
+            startDestination = "speakers"
+        ) {
+            composable(("speakers")) {
+                SpeakerFeed() { speaker ->
+                    navController.navigate("speaker/${speaker.id}") {
+                        popUpTo(navController.graph.findStartDestination().id)
+                    }
+                }
+            }
+            composable(("speaker/{speakerId}")) { backStackEntry ->
+                SpeakerProfileScreen(
+                    backStackEntry.arguments?.getString("speakerId")
+                )
+            }
+        }
+    }
 }
